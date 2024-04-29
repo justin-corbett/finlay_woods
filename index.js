ec2b3c37c23663f0024be51afd616f2fae1bbb36
JC <120233340+justjuicenom@users.noreply.github.com>
2024-04-29 20:59:02 +1000

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ ScrollTrigger.defaults({
   markers: false,
 });
 
+/*
 // Image scale full projects
 $(".section-spacer-projects_img").each(function (index) {
   let triggerElement = $(this);
@@ -44,7 +45,6 @@ $(".section-spacer-about_img").each(function (index) {
   });
 });
 
-
 // About image scale (with scrubbing)
 $(".about-image-wrapper").each(function (index) {
   let triggerElement = $(this);
@@ -64,6 +64,7 @@ $(".about-image-wrapper").each(function (index) {
     duration: 1,
   });
 });
+*/
 
 // Logo Scale
 $(".section-hero_home").each(function (index) {
@@ -82,6 +83,7 @@ $(".section-hero_home").each(function (index) {
   tl.to(targetElement, {
     y: "0%",
     duration: 1,
+    ease: 'power3.out',
   });
 });
 
@@ -99,10 +101,11 @@ $(".section-hero_home").each(function (index) {
     },
   });
   tl.to(targetElement, {
-    fontSize: "1.25EM",
+    fontSize: "2EM",
     duration: 1,
   });
 });
+
 
 // Loader 
 window.addEventListener("load", function() {
@@ -125,6 +128,83 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 window.addEventListener("load", loader);
+
+/*
+function loader() {
+
+  // Add a delay before stopping the scroll (adjust the delay time as needed)
+  setTimeout(function() {
+    window.SScroll.call.stop();
+  }, 1000); // 1000 milliseconds = 1 second
+  
+  // get references to the elements we want to animate
+  const titleTexts = document.querySelectorAll(".loader-title");
+  const titleWrap = titleTexts[0].parentNode;
+  const loaderBackground = document.querySelectorAll(".loader_background");
+
+  const tl = gsap.timeline({
+    onComplete: () => {
+      // allow scroll when animation completes
+      gsap.set("body", { overflow: "auto" });
+      gsap.set(".loader", { display: "none" });
+      window.SScroll.call.start();
+    },
+  });
+
+  // prevent flash of .main container on load
+  // works in tandem with <style> in page settings code
+  gsap.set(".main-wrapper", { visibility: "visible" });
+  gsap.set(".loader-text", { y: "100%" })
+  
+  gsap.registerEffect({
+    name: "swingUp",
+    effect: (targets, config) => {
+      return gsap.from(targets, {
+        duration: config.duration,
+        autoAlpha: 0, // FOUC - visibility -> visible when opacity > 0
+        yPercent: 100,
+        rotateZ: 0,
+        stagger: 0.2,
+        ease: 'power3.out',
+      });
+    },
+  	defaults: { duration: 0.5 },
+  	extendTimeline: true, // allows nice timeline syntax
+	});
+
+  tl
+  	.to(".loader-text", {
+    y: "-100%",
+    duration: 0.2,
+    ease: 'power3.out'
+  	})
+    // swing up "RESTOCK"
+    .swingUp(titleTexts[0])
+    // move title wrap up 33% y direction
+    .fromTo(titleWrap, { yPercent: 66 }, { yPercent: 33, duration: 0.5, ease: 'power3.out' })
+    // swing up "REORDER"
+    .swingUp(titleTexts[1],  "<+0.1")
+    
+    // move title wrap up to its zero position
+    .to(titleWrap, { yPercent: 0, duration: 1, ease: 'power3.out' }, "<+0.5")
+    // swing up "REPEAT"
+    .swingUp(titleTexts[2], { duration: 1 }, "<+0.1")
+    // bring main container into view and animate border-radius
+    .to(".loader_background", {
+      yPercent: -100,
+      duration: 0.6,
+      delay: 0.8,
+      ease: 'power3.out',
+    })
+    .to(".loader-title", {
+      autoAlpha: 0,
+      duration: 0.2,
+      delay: 0,
+      ease: 'power3.out',
+    }, "<+0.05")
+  
+}
+*/
 
 function loader() {
 
@@ -252,7 +332,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
     });
   });
 });
-</script>
+
 
 // Projects hover, add class
 document.addEventListener('DOMContentLoaded', () => {
@@ -269,3 +349,75 @@ document.addEventListener('DOMContentLoaded', () => {
   });
  }); 
 });
+
+// Horizontal scroll
+let tlMain = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".section-height",
+      start: "top top",
+      end: "98% bottom",
+      scrub: 1
+    }
+  })
+  .to(".track", {
+    xPercent: -100,
+    ease: "none"
+  });
+  
+// hero photo
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".hero-panel",
+      containerAnimation: tlMain,
+      start: "left right",
+      end: "right left",
+      scrub: true
+    }
+  })
+  .to(".hero-panel_img", { scale: 1 }, 0);
+  
+
+// Optional - Set sticky section heights based on inner content width
+// Makes scroll timing feel more natural
+function setTrackHeights() {
+  $(".section-height").each(function (index) {
+    let trackWidth = $(this).find(".track").outerWidth();
+    $(this).height(trackWidth);
+  });
+}
+setTrackHeights();
+window.addEventListener("resize", function () {
+  setTrackHeights();
+});
+
+// Scroll animation for projects background
+gsap.timeline({
+  scrollTrigger: {
+      trigger: ".section-height",
+      start: "top top",
+      end: "35% bottom",
+      scrub: 1
+  }
+})
+.to(".projects-bg-slide", {
+  x: 0,
+  ease: "power1.out" // You can adjust the ease as needed
+});
+
+// Scroll animation for about background
+gsap.timeline({
+  scrollTrigger: {
+      trigger: ".scroll-trigger-about",
+      start: "top top",
+      end: "bottom 80%",
+      scrub: 1
+  }
+})
+.to(".about-bg-slide", {
+  y: 0,
+  ease: "none"
+ // You can adjust the ease as needed
+});
+
